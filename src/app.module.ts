b38423c9a67950bb7d26d937d387b5d0ae8a1fe2
DesarrolloP6 <desarrollo6.glabs@outlook.com>
2024-01-19 07:35:07 -0600

    Clase 104 - Config Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,19 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/app.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load:[EnvConfiguration]
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule
@@ -19,4 +25,9 @@ import { SeedModule } from './seed/seed.module';
 
 
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    console.log(process.env);
+    
+  }
+}
